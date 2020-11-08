05bffa308ea12e50af6c64cff78244ae7d4d7cc2
EQ <fleflefle1991@gmail.com>
2020-11-09 03:48:51 +0900

    토큰 소지 여부 확인 후 Retrofit의 header에 token을 넣는지 분기.

--- app/src/main/java/com/soldemom/outstagram/MasterApplication.kt
+++ app/src/main/java/com/soldemom/outstagram/MasterApplication.kt
@@ -1,9 +1,11 @@
 package com.soldemom.outstagram
 
 import android.app.Application
+import android.content.Context
 import com.facebook.stetho.okhttp3.StethoInterceptor
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -12,12 +14,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MasterApplication : Application() {
 
     lateinit var service: RetrofitService
+    var token: String? = null
 
     //manifest상에서 application이 먼저 실행됨.
     //application의 name속성에 .MasterApplication을 입력하면 application으로 MasterApplication이 실행됨.
     //여기의 onCreate에서 특정 객체를 생성하면 모든 액티비티에서 이용가능함.
     override fun onCreate() {
         super.onCreate()
+
+        createRetrofit()
 
 
         //stetho, glide, retrofit, gson,
@@ -28,10 +33,19 @@ class MasterApplication : Application() {
 
         val header = Interceptor {  //휴대폰에서부터 통신이 나갈때 그걸 가로챈 후
             val original = it.request()  //request를 받아 original이란 변수에 넣고
-            val request = original.newBuilder()  //original 개조 시작
-                .header("Authorization","") //original에 header를 달아줌
-                .build()
-            it.proceed(request)     //모든 개조 완료된 이후 다시 내보내줌
+
+            val requestBuilder = original.newBuilder()
+            val request : Request
+
+            if (checkIsLogin()) {
+                //로그인이 되어있다면
+                token?.let {
+                    requestBuilder.header("Authorization",token) //original에 header를 달아줌
+                }
+            }   //로그인이 되어있지 않다면 header를 달지 않고 바로 넘어감
+            request = requestBuilder.build()
+
+            it.proceed(request)     //개조 완료된 이후 다시 내보내줌
         }
 
         val client = OkHttpClient.Builder()
@@ -52,9 +66,13 @@ class MasterApplication : Application() {
     // 로그인을 하지 않았다면 달아줄 수 있는 token이 없으므로
     // SharedPreference에 token이 존재하는지 여부를 확인
     fun checkIsLogin() : Boolean {
+        token = getUserToken()
+        return token != "null"
+    }
 
-
-        return false
+    fun getUserToken(): String? {
+        val sp = getSharedPreferences("login_sp", Context.MODE_PRIVATE)
+        return sp.getString("login_sp","null")
     }
 
 
